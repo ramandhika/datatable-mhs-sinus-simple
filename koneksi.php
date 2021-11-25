dc74c0598b1a83aa0116bc92a99f413cc81109af
Ramandhika IP <ramandhika.ilham@gmail.com>
2021-11-25 21:47:34 +0700

    1st Commit

--- koneksi.php
+++ koneksi.php
@@ -1,6 +1,6 @@
 <?php
 //variabel koneksi
-$konek = mysqli_connect("localhost", "id18006964_rama", "Ramapolisi86.", "id18006964_datamahasiswa");
+$konek = mysqli_connect("localhost", "root", "", "data-mahasiswa-sinus");
 
 if (!$konek) {
     echo "Koneksi Database Gagal...!!!";
